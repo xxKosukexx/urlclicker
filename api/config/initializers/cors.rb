3bb4381190fed7e9d3f931dd734dd903312c3215
xxKosukexx <clover1111bag@gmail.com>
2025-09-11 20:54:45 +0900

    環境構築 - 起動まで

---
+++ api/config/initializers/cors.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins ENV.fetch("CORS_ORIGIN","*")
+    resource "*", headers: :any, methods: :any
+  end
+end
---
+++ api/config/puma.rb
@@ -0,0 +1,2 @@
+port ENV.fetch("PORT", 3000)
+bind "tcp://0.0.0.0:3000"
